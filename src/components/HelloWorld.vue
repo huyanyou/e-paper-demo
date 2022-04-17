92e2b3926fa30dafeaaaac951c8afd00cd465c5c
huyanyou <1026855639@qq.com>
2022-04-17 14:01:54 +0800

    book demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,74 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div id="top">
+      <img src="../assets/logo.png" alt="logo">
+      <span id="top-title">墨水屏群控管理演示</span>
+    </div>
+    <div>
+      <div class="spacing">
+      </div>
+      <div id="content">
+        <table border="0" width="100%" cellpadding="0px">
+          <tr>
+            <th align="left">屏幕型号</th>
+            <th align="left">设备IP</th>
+            <th align="left">设备MAC</th>
+            <th align="left">SSID</th>
+            <th align="left">信号强度</th>
+            <th align="left">电池电量</th>
+            <th align="left">模板编号</th>
+            <th align="left">显示内容</th>
+            <th align="left">内容状态</th>
+            <th align="left" style="width:10%">上传时间</th>
+            <th align="left" style="width:10%">最后上线时间</th>
+            <th align="left" style="width:15%">操作</th>
+          </tr>
+          <tr>
+            <td>E-ink 75b</td>
+            <td>192.168.1.3</td>
+            <td>a0:78:17:7d:76:e0</td>
+            <td>singzer</td>
+            <td>-75db</td>
+            <td>68%</td>
+            <td>01</td>
+            <td>测试会议</td>
+            <td>已上传</td>
+            <td>2022/3/23 16:36:18</td>
+            <td>2022/3/23 16:36:18</td>
+            <td>
+              <span>编辑</span>
+              <span>编辑</span>
+              <span>编辑</span>
+            </td>
+          </tr>
+          <tr>
+            <td>E-ink 75b</td>
+            <td>192.168.1.3</td>
+            <td>a0:78:17:7d:76:e0</td>
+            <td>singzer</td>
+            <td>-75db</td>
+            <td>68%</td>
+            <td>01</td>
+            <td>测试会议</td>
+            <td>等待设备唤醒</td>
+            <td>2022/3/23 16:36:18</td>
+            <td>2022/3/23 16:36:18</td>
+            <td>
+              <span>编辑</span>
+              <span>编辑</span>
+              <span>编辑</span>
+            </td>
+          </tr>
+        </table>
+        <div id="content-btn">
+          <button>批量编辑内容</button>
+          <button>批量上传</button>
+        </div>
+      </div>
+      <div class="spacing">
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,18 +83,78 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  img {
+    height: 8rem;
+    width: 8rem;
+    border-radius: 50%;
+    margin-left: 6rem;
+  }
+
+  table {
+    margin-left: 5rem;
+    margin-right: 5rem;
+  }
+
+  th {
+    font-size: 3rem;
+    color: #90939F;
+  }
+
+  td {
+    font-size: 3rem;
+    color: black;
+    height: 6rem;
+  }
+
+  tr:not(:first-child):hover {
+    background-color: #F5F5F5;
+  }
+
+  #top {
+    background-color: #409EEF;
+    height: 10rem;
+  }
+
+  #content-btn{
+    float: right;
+    margin-top: 2rem;
+  }
+
+  button {
+    background-color: #409EEF;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    /* height: 4rem; */
+    /* width: 8rem; */
+    /* margin-left: 1rem;
+    margin-right: 1rem; */
+    padding: 1rem 3rem;
+    font-size: 3rem;
+  }
+
+  button:first-child{
+    margin-right: 5rem;
+  }
+
+  button:not(:first-child) {
+    margin-right: 18rem;
+  }
+
+  .spacing{
+    height: 5rem;
+    background-color: #F5F7FA;
+    width: 100%;
+  }
+  .spacing:nth-child(3){
+    position: absolute;
+    bottom: 0;
+  }
+  #top-title {
+    color: #FFFFFF;
+    font-size: 4rem;
+    margin-left: 6rem;
+    position: relative;
+    top: -2rem;
+  }
 </style>
